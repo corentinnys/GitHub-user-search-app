b6f7db3a5d3421f57b022971596f2d74f3c2efad
Corentin <corentinys@gmail.com>
2026-03-19 09:07:05 +0100

    correction css html

--- scripts/request.js
+++ scripts/request.js
@@ -5,12 +5,77 @@ $('#search').click(function (e) {
         url: 'https://api.github.com/users/' + value,
         method: 'GET',
         success: function (data) {
+
+            const dateString = data.created_at;
+
+            const date = new Date(dateString);
+
+            const options = {
+                day: "2-digit",
+                month: "short",
+                year: "numeric"
+            };
+
+            const formattedDate = date.toLocaleDateString("en-GB", options);
+
+
+
             $('#name').text(data.login);
             $("#repos").text(data.public_repos)
             $("#followers").text(data.followers)
             $('.user-avatar').attr('src',data.avatar_url)
+            $('.profile-avatar-small').attr('src',data.avatar_url)
             $('#website').attr('href',data.html_url)
-            console.log(data);
+            $('#following').text(data.following)
+             if (data.location == null)
+             {
+                 $('#location').text('pas de localisation')
+             }else{
+                 $('#location').text(data.location)
+             }
+            if (data.bio == null)
+            {
+                $('#bio').text('pas de biographie')
+            }else{
+                $('#bio').text(data.bio)
+            }
+            $('#username').text(data.name)
+            $('#joined').text(formattedDate)
+
         }
     })
+})
+const path = $('.theme-button svg path');
+$('.theme-button').click(function (e){
+    e.preventDefault();
+    if ($(this).attr('data-theme')==="black"){
+        $(this).attr('data-theme','light')
+        $('body').removeClass('light-mode').addClass('dark-mode')
+        $('.header-title').css('color','white');
+        $('.user-card').css('background-color','#1e2a47');
+        $('.stats-container').css('background','black');
+        $('.stat-item').css('color','white');
+        $('.icon').removeClass('icon-dark').addClass('icon-light');
+        $('.search-bar').removeClass('search-bar-light').addClass('search-bar-dark');
+        $('.info-text').css('color','white');
+        $('.info-link').css('color','white');
+        path.attr('d', 'M19.513 11.397a.701.701 0 00-.588.128 7.496 7.496 0 01-2.276 1.336 7.101 7.101 0 01-2.583.462 7.505 7.505 0 01-5.32-2.209 7.568 7.568 0 01-2.199-5.342c0-.873.154-1.72.41-2.49a6.904 6.904 0 011.227-2.21.657.657 0 00-.102-.924.701.701 0 00-.589-.128C5.32.61 3.427 1.92 2.072 3.666A10.158 10.158 0 000 9.83c0 2.8 1.125 5.342 2.967 7.19a10.025 10.025 0 007.16 2.98c2.353 0 4.527-.822 6.266-2.183a10.13 10.13 0 003.58-5.624.623.623 0 00-.46-.796z" fill="#697C9A" fill-rule="nonzero"');
+
+    }else{
+
+        $(this).attr('data-theme','black')
+        $('body').removeClass('dark-mode').addClass('light-mode')
+        $('.header-title').css('color','black');
+        $('.user-card').css('background-color','white');
+        $('.stats-container').css('background','gray');
+        $('.stat-item').css('color','black');
+        $('.icon').removeClass('icon-light').addClass('icon-dark');
+        $('.search-bar').removeClass('search-bar-dark').addClass('search-bar-light');
+        $('.info-text').css('color','black');
+        $('.info-link').css('color','black');
+        path.attr('d', 'M13.545 6.455c-.9-.9-2.17-1.481-3.545-1.481a4.934 4.934 0 00-3.545 1.481c-.9.9-1.481 2.17-1.481 3.545 0 1.376.582 2.646 1.481 3.545.9.9 2.17 1.481 3.545 1.481a4.934 4.934 0 003.545-1.481c.9-.9 1.481-2.17 1.481-3.545a4.934 4.934 0 00-1.481-3.545zM10 3.413a.7.7 0 00.688-.688V.688A.7.7 0 0010 0a.7.7 0 00-.688.688v2.037a.7.7 0 00.688.688zM15.635 5.344l1.455-1.455a.67.67 0 000-.952.67.67 0 00-.952 0l-1.455 1.455a.67.67 0 000 .952c.238.264.66.264.952 0zM19.312 9.312h-2.037a.7.7 0 00-.688.688.7.7 0 00.688.688h2.037A.7.7 0 0020 10a.7.7 0 00-.688-.688zM15.608 14.656a.67.67 0 00-.952 0 .67.67 0 000 .952l1.455 1.455a.67.67 0 00.952 0 .67.67 0 000-.952l-1.455-1.455zM10 16.587a.7.7 0 00-.688.688v2.037A.7.7 0 0010 20a.7.7 0 00.688-.688v-2.037a.7.7 0 00-.688-.688zM4.365 14.656L2.91 16.111a.67.67 0 000 .952.67.67 0 00.952 0l1.455-1.455a.67.67 0 000-.952c-.238-.264-.66-.264-.952 0zM3.413 10a.7.7 0 00-.688-.688H.688A.7.7 0 000 10a.7.7 0 00.688.688h2.037A.7.7 0 003.413 10zM4.365 5.344a.67.67 0 00.952 0 .67.67 0 000-.952L3.862 2.937a.67.67 0 00-.952 0 .67.67 0 000 .952l1.455 1.455z ');
+        path.attr('fill', 'black');
+    }
+
+
 })
